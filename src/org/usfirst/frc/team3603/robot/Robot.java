f8b028e95f957165d50eb83d880f65914202c29a
Connor <Connor@192.168.1.108>
2017-10-09 07:51:18 -0400

    Added encoder and navX code.

--- src/org/usfirst/frc/team3603/robot/Robot.java
+++ src/org/usfirst/frc/team3603/robot/Robot.java
@@ -1,80 +1,61 @@
 package org.usfirst.frc.team3603.robot;
 
+import com.ctre.CANTalon;
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot {
-	final String defaultAuto = "Default";
-	final String customAuto = "My Auto";
-	String autoSelected;
-	SendableChooser<String> chooser = new SendableChooser<>();
-
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+	
+	AHRS navx = new AHRS(SerialPort.Port.kMXP);
+	MyEncoder enc = new MyEncoder(0);
+	
+	CANTalon frontLeft = new CANTalon(0);
+	CANTalon backLeft = new CANTalon(1);
+	CANTalon frontRight = new CANTalon(2);
+	CANTalon backRight = new CANTalon(3);
+	RobotDrive mainDrive = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
+	
+	Joystick joy1 = new Joystick(0);
+	
 	@Override
 	public void robotInit() {
-		chooser.addDefault("Default Auto", defaultAuto);
-		chooser.addObject("My Auto", customAuto);
-		SmartDashboard.putData("Auto choices", chooser);
 	}
 
-	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional comparisons to the
-	 * switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
 	@Override
 	public void autonomousInit() {
-		autoSelected = chooser.getSelected();
-		// autoSelected = SmartDashboard.getString("Auto Selector",
-		// defaultAuto);
-		System.out.println("Auto selected: " + autoSelected);
 	}
 
-	/**
-	 * This function is called periodically during autonomous
-	 */
 	@Override
 	public void autonomousPeriodic() {
-		switch (autoSelected) {
-		case customAuto:
-			// Put custom auto code here
-			break;
-		case defaultAuto:
-		default:
-			// Put default auto code here
-			break;
-		}
 	}
-
-	/**
-	 * This function is called periodically during operator control
-	 */
+	
 	@Override
 	public void teleopPeriodic() {
 	}
-
-	/**
-	 * This function is called periodically during test mode
-	 */
+	
 	@Override
 	public void testPeriodic() {
+	}
+	
+	public void read() {
+		SmartDashboard.putNumber("getAngle", navx.getAngle());
+		SmartDashboard.putNumber("getRate", navx.getRate());
+		SmartDashboard.putNumber("getDisplacementX", navx.getDisplacementX());
+		SmartDashboard.putNumber("getDisplacementY", navx.getDisplacementY());
+		SmartDashboard.putNumber("getDisplacementZ", navx.getDisplacementZ());
+		SmartDashboard.putNumber("getRawAccelX", navx.getRawAccelX());
+		SmartDashboard.putNumber("getRawAccelY", navx.getRawAccelY());
+		SmartDashboard.putNumber("getRawAccelZ", navx.getRawAccelZ());
+		SmartDashboard.putNumber("getVelocityX", navx.getVelocityX());
+		SmartDashboard.putNumber("getVeloctiyY", navx.getVelocityY());
+		SmartDashboard.putNumber("getVelocityZ", navx.getVelocityZ());
+		SmartDashboard.putBoolean("isRotating", navx.isRotating());
+		SmartDashboard.putBoolean("isMoving", navx.isMoving());
 	}
 }
 
