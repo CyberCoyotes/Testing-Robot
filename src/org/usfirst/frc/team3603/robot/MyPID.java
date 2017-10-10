7dfd08981afd4e298c892aacb809b913f0af9789
PorkyPig <PorkyPig@LAPTOP-VDC0L2M7>
2017-10-10 16:59:06 -0400

    x

--- src/org/usfirst/frc/team3603/robot/MyPID.java
+++ src/org/usfirst/frc/team3603/robot/MyPID.java
@@ -8,40 +8,41 @@ public class MyPID {
 	private double m_i;
 	private double m_d;
 	
-	public MyPID(int p, int i, int d) {
+	public MyPID(double p, double i, double d) {
 		total = 0;
 		m_p = p;
 		m_i = i;
 		m_d = d;
 	}
 	
-	public double getSpeed(double offness, double time, double change) {
-		double P = m_p * 1/Math.pow(offness, 2) * offness;
+	public double getSpeed(double offness) {
+		//double P = m_p * 1/Math.pow(offness, 2) * offness;
 		
-		/*
-		double P = p * Math.pow(offness, 3);
-		*/
+		
+		double P = m_p * Math.pow(offness, 3);
+		
 		
 		total = offness + total;
 		double I = total * m_i;
 		
+		/*
 		time = Math.abs(time-m_time);
-		change = Math.abs(change-m_change);
-		double D = m_d * (time/change);
+		offness = Math.abs(offness-m_change);
+		double D = m_d * (time/offness);
 		
+		/*
 		double PID = P + I + D;
+		*/
 		
-		if(PID > 1) {
-			PID = 1;
-		} else if(PID < -1) {
-			PID = -1;
+		double yes = P+I;
+		
+		if(yes > 1) {
+			yes = 1;
+		} else if(yes < -1) {
+			yes = -1;
 		}
 		
-		return PID;
-	}
-	
-	public void preD(double time, double change) {
-		m_time = time;
-		m_change = change;
+		
+		return -yes;
 	}
 }
--- src/org/usfirst/frc/team3603/robot/Robot.java
+++ src/org/usfirst/frc/team3603/robot/Robot.java
@@ -3,32 +3,40 @@ package org.usfirst.frc.team3603.robot;
 import com.ctre.CANTalon;
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 	
-	AHRS navx = new AHRS(SerialPort.Port.kMXP);
-	MyEncoder enc = new MyEncoder(0);
+	ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+	MyPID gy = new MyPID(0.05, 0.005, 0.0);
 	
-	CANTalon frontLeft = new CANTalon(0);
-	CANTalon backLeft = new CANTalon(1);
-	CANTalon frontRight = new CANTalon(2);
-	CANTalon backRight = new CANTalon(3);
+	Victor backLeft = new Victor(1);
+	Victor backRight = new Victor(2);
+	Victor frontLeft = new Victor(3);
+	Victor frontRight = new Victor(4);
 	RobotDrive mainDrive = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
 	
 	Joystick joy1 = new Joystick(0);
+	Timer timer = new Timer();
 	
 	@Override
 	public void robotInit() {
+		timer.start();
+		backRight.setInverted(true);
+    	frontRight.setInverted(true);
 	}
 
 	@Override
 	public void autonomousInit() {
-	}
+		
+	}//
 
 	@Override
 	public void autonomousPeriodic() {
@@ -36,6 +44,14 @@ public class Robot extends IterativeRobot {
 	
 	@Override
 	public void teleopPeriodic() {
+		if(joy1.getRawButton(1)) {
+			//gy.preD(timer.get(), gyro.getAngle());
+			mainDrive.mecanumDrive_Cartesian(0, 0, gy.getSpeed(gyro.getAngle()), 0);
+			SmartDashboard.putNumber("gy", gy.getSpeed(gyro.getAngle()));
+			SmartDashboard.putNumber("gyro", gyro.getAngle());
+		} else {
+			//mainDrive.mecanumDrive_Cartesian(0, 0, 0, 0);
+		}
 	}
 	
 	@Override
@@ -43,6 +59,7 @@ public class Robot extends IterativeRobot {
 	}
 	
 	public void read() {
+		/*
 		SmartDashboard.putNumber("getAngle", navx.getAngle());
 		SmartDashboard.putNumber("getRate", navx.getRate());
 		SmartDashboard.putNumber("getDisplacementX", navx.getDisplacementX());
@@ -56,6 +73,7 @@ public class Robot extends IterativeRobot {
 		SmartDashboard.putNumber("getVelocityZ", navx.getVelocityZ());
 		SmartDashboard.putBoolean("isRotating", navx.isRotating());
 		SmartDashboard.putBoolean("isMoving", navx.isMoving());
+		*/
 	}
 }
 
