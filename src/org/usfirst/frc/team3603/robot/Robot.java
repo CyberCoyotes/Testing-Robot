ab0eab7c799604732152ca2a62c3c62a35fa0860
PorkyPig <PorkyPig@LAPTOP-VDC0L2M7>
2017-10-10 21:00:39 -0400

    Began auton switcher.

--- src/org/usfirst/frc/team3603/robot/Robot.java
+++ src/org/usfirst/frc/team3603/robot/Robot.java
@@ -4,6 +4,8 @@ import com.ctre.CANTalon;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DigitalOutput;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
@@ -16,6 +18,10 @@ public class Robot extends IterativeRobot {
 	
 	ADXRS450_Gyro gyro = new ADXRS450_Gyro();
 	MyPID gy = new MyPID(0.05, 0.005, 0.0);
+	DigitalOutput autonOut = new DigitalOutput(0);
+	DigitalInput in1 = new DigitalInput(0);
+	DigitalInput in2 = new DigitalInput(1);
+	DigitalInput in3 = new DigitalInput(2);
 	
 	Victor backLeft = new Victor(1);
 	Victor backRight = new Victor(2);
@@ -31,6 +37,8 @@ public class Robot extends IterativeRobot {
 		timer.start();
 		backRight.setInverted(true);
     	frontRight.setInverted(true);
+    	
+    	autonOut.set(true);
 	}
 
 	@Override
@@ -51,6 +59,15 @@ public class Robot extends IterativeRobot {
 			SmartDashboard.putNumber("gyro", gyro.getAngle());
 		} else {
 			//mainDrive.mecanumDrive_Cartesian(0, 0, 0, 0);
+		}
+		
+		if(in1.get()) {
+			SmartDashboard.putNumber("1", 1);
+		} else if(in2.get()) {
+			SmartDashboard.putNumber("1", 2);
+		}
+		else if(in3.get()) {
+			SmartDashboard.putNumber("1", 3);
 		}
 	}
 	
