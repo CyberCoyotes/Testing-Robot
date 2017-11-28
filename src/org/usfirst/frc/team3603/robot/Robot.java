847dfdaec54c79e46bd979dfcfa04dc9bc9f79cd
Connor <Connor@3WJ4TZ1>
2017-11-28 09:04:49 -0500

    Working on creating Threads.

--- src/org/usfirst/frc/team3603/robot/Robot.java
+++ src/org/usfirst/frc/team3603/robot/Robot.java
@@ -2,17 +2,24 @@ package org.usfirst.frc.team3603.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
+	
 	Victor backLeft = new Victor(1);
 	Victor backRight = new Victor(2);
 	Victor frontLeft = new Victor(3);
 	Victor frontRight = new Victor(4);
 	
+	RobotDrive mainDrive = new RobotDrive(frontLeft, backLeft, frontRight, backRight);
+	
+	Vision vision = new Vision();
 	Joystick joy1 = new Joystick(0);
 	Timer timer = new Timer();
+	MyPID pid = new MyPID(0.0005, 0);
 	
 	@Override
 	public void robotInit() {
@@ -32,6 +39,14 @@ public class Robot extends IterativeRobot {
 	
 	@Override
 	public void teleopPeriodic() {
+		if(joy1.getRawButton(1)) {
+			double rotation = pid.getPID(vision.getSpeed());
+			mainDrive.mecanumDrive_Cartesian(0, 0, rotation, 0);
+		} else {
+			mainDrive.mecanumDrive_Cartesian(0, 0, 0, 0);
+		}
+		
+		read();
 	}
 	
 	@Override
@@ -39,6 +54,9 @@ public class Robot extends IterativeRobot {
 	}
 	
 	public void read() {
+		SmartDashboard.putNumber("Vision speed", vision.getSpeed());
+		SmartDashboard.putNumber("PID", pid.getPID(vision.getSpeed()));
+		SmartDashboard.putString("Keys", vision.getKeys());
 	}
 }
 
